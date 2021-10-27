3ef029b9bc4eea985d59691d0ce1746e1899e824
Valentin <navshop@ukr.net>
2021-10-27 23:00:58 +0300

    add hm_1

---
+++ Curs1/lesson_2.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import pandas as pd
+
+s = 'ss'
+lst2 = [x for x in range(1001)]
+lst3 = [str(x) for x in range(0, 1000)]
+a0 = 100
+a1 = 101
+
+a0 > a1
+a1 == a0
+mediana = np.median(lst2)
+lts4 = [x for x in lst2 if x >= mediana] # поиск чисел больше медианы
